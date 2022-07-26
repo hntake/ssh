555c9262de3296e651ff82fd44d3d336e2234952
hntake <hntake@gmail.com>
2022-07-26 23:01:55 +0900

    07-26mypicture大文字に修正

--- app/Http/Controllers/RankController.php
+++ app/Http/Controllers/RankController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Word;
-use App\Models\NIce;
+use App\Models\Nice;
 
 
 class RankController extends Controller
