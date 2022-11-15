399480cd9909fea881123d67ed1e885751d78fdf
hntake <hntake@gmail.com>
2022-11-15 15:12:07 +0900

    11-15caseADD

--- app/Http/Controllers/FormController.php
+++ app/Http/Controllers/FormController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Form;
 use App\Models\News;
+use App\Models\Cases;
 
 class FormController extends Controller
 {
@@ -18,6 +19,7 @@ class FormController extends Controller
         }
        }
  */
+/**ブログ作成ページへ */
     public function wys (Request $request){
         if ($request->user('admin')?->admin_level === 10) {
 
@@ -27,6 +29,7 @@ class FormController extends Controller
         }
 
       }
+/*お知らせ作成ページへ*/
     public function news (Request $request){
         if ($request->user('admin')?->admin_level === 10) {
 
@@ -36,6 +39,17 @@ class FormController extends Controller
             return view('adminLogin');
         }
       }
+/*事象作成ページへ*/
+    public function case (Request $request){
+        if ($request->user('admin')?->admin_level === 10) {
+
+            return view ('/case/wysiwyg');
+        }
+        else{
+            return view('adminLogin');
+        }
+      }
+/*ブログ保存*/
     public function savenew (Request $request){
         $post = new Form;
         $post->title = $request->title;
@@ -54,7 +68,27 @@ class FormController extends Controller
 
         return redirect ('/blog/index');
        }
+/*事象保存*/
+    public function savecase (Request $request){
+        $post = new Cases;
+        $post->title = $request->title;
+        $post->main = $request->main;
+        $post->category = $request->category;
+        $post->save();
 
+        // 画像の保存
+    if($request->post_img){
+
+        if($request->post_img->extension() == 'gif' || $request->post_img->extension() == 'jpeg' || $request->post_img->extension() == 'jpg' || $request->post_img->extension() == 'png'){
+        $request->file('post_img')->storeAs('public/post_img', $post->id.'.'.$request->post_img->extension());
+    }
+
+}
+
+        return redirect ('/case/index');
+       }
+
+/*お知らせ保存*/
     public function save_news (Request $request){
         $post = new News;
         $post->title = $request->title;
@@ -66,7 +100,7 @@ class FormController extends Controller
 
         return redirect ('/news/index');
        }
-
+/*ブログトップページ表示*/
      public function index (Request $request){
         $data = Form::orderBy('created_at', 'desc')->paginate(10);
         $eng = Form::where('category','=', '4')->orderBy('created_at', 'desc')->paginate(10);
@@ -79,6 +113,20 @@ class FormController extends Controller
              'etc'   => $etc,
         ]);
       }
+/*事象トップページ表示*/
+     public function caseindex (Request $request){
+        $data = Cases::orderBy('created_at', 'desc')->paginate(10);
+        $eng = Cases::where('category','=', '4')->orderBy('created_at', 'desc')->paginate(10);
+        $vs = Cases::where('category','=', '5')->orderBy('created_at', 'desc')->paginate(10);
+        $etc = Cases::where('category','=', '6')->orderBy('created_at', 'desc')->paginate(10);
+        return view('/case/post')->with(
+            ['data' => $data,
+             'eng'  => $eng,
+             'vs'   => $vs,
+             'etc'   => $etc,
+        ]);
+      }
+/*お知らせトップページ表示*/
      public function news_index (Request $request){
         $data = News::orderBy('created_at', 'desc')->paginate(10);
         $service = News::where('category','=', '7')->orderBy('created_at', 'desc')->paginate(10);
@@ -93,7 +141,7 @@ class FormController extends Controller
              'lelease'   => $lelease,
         ]);
       }
-
+/*選択したブログページ表示*/
      public function page (Request $request,$id){
         $data = Form::where('id', $request->id)->first();
         return view('blog/page', [
@@ -101,10 +149,18 @@ class FormController extends Controller
             'data' => $data,
         ]);
       }
-
+/*選択したお知らせページ表示*/
      public function news_page (Request $request,$id){
         $data = News::where('id', $request->id)->first();
         return view('news/page', [
+            'id' => $id,
+            'data' => $data,
+        ]);
+      }
+/*選択した事象ページ表示*/
+     public function case_page (Request $request,$id){
+        $data = Cases::where('id', $request->id)->first();
+        return view('case/page', [
             'id' => $id,
             'data' => $data,
         ]);
---
+++ app/Models/Cases.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Category;
+
+
+class Cases extends Model
+{
+    use HasFactory;
+        /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'title','main','category',
+
+    ];
+
+    public function Category() {
+        return $this->hasOne(Category::class, 'id','category');
+    }
+
+}
