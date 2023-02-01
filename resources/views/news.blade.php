ac557fdce7aef7d69a65913e18c514d27a94bee8
hntake <hntake@gmail.com>
2023-02-01 10:24:41 +0900

    2－1お知らせ修正

--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>ブログ｜webアプリ制作会社llco</title>
+    <title>おしらせ｜webアプリ制作会社llco</title>
     <link rel="stylesheet" href="{{asset('../css/news.css')}}">
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
