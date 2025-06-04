e91c00c8ebfc1cbe866adf20a674a162158d740a
hntake <hntake@gmail.com>
2025-06-05 06:48:02 +0900

    ６−４ログイン修正

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 class LoginController extends Controller
 {
@@ -28,8 +30,7 @@ class LoginController extends Controller
      */
 // LoginController.php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
+
 
 protected function authenticated(Request $request, $user)
 {
